d0de60b69e0b13e0a97207bc986d465d74f513cb
andywar65 <andy.war1965@gmail.com>
2019-11-10 21:57:05 +0100

    addressing settings in wsgi and manage

--- rpnew_prog/wsgi.py
+++ rpnew_prog/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpnew_prog.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rpnew_prog.settings.dev')
 
 application = get_wsgi_application()
