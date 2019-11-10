14c1995956b6c08f477c74d2e1dbd3a152deed38
andywar65 <andy.war1965@gmail.com>
2019-11-10 23:30:25 +0100

    templates for utente login logout signup

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
 
-# Create your views here.
+from .forms import UtenteCreationForm
+
+class SignUpView(CreateView):
+    form_class = UtenteCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'signup.html'
