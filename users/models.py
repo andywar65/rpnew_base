ba729a26c40a380fbcf10455669e17c05f212c44
andywar65 <andy.war1965@gmail.com>
2019-11-11 21:32:37 +0100

    added the member model

--- users/models.py
+++ users/models.py
@@ -2,4 +2,16 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
-    pass
+    def save(self, *args, **kwargs):
+        super(User, self).save(*args, **kwargs)
+        try:
+            memb = Member.objects.get(user_id = self.id)
+            return
+        except:
+            memb = Member.objects.create(user = self, )
+            memb.save()
+            return
+
+class Member(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+        primary_key=True, editable=False)
