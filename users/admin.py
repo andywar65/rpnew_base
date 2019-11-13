c6eead64edf276ef151bf6e5c70c7677bf2b6ff8
andywar65 <andy.war1965@gmail.com>
2019-11-13 17:22:19 +0100

    little bug fix in users admin

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User, Member
+from .models import User, Member, CourseSchedule
 
 admin.site.register(User, UserAdmin)
 
@@ -9,3 +9,7 @@ class MemberAdmin(admin.ModelAdmin):
     list_display = ('get_full_name', 'sector', 'parent', 'mc_state', 'settled')
     search_fields = ('user__first_name', 'user__last_name',
         'user__username', 'fiscal_code', 'address')
+
+@admin.register(CourseSchedule)
+class CourseScheduleAdmin(admin.ModelAdmin):
+    list_display = ('full', )
