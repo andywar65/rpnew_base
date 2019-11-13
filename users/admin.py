cdc5550dfd7e9e1fe281f72236aa5d8b71d39259
andywar65 <andy.war1965@gmail.com>
2019-11-13 22:12:28 +0100

    formsets depend on sector, parent, permission

--- users/admin.py
+++ users/admin.py
@@ -41,12 +41,73 @@ class MemberAdmin(admin.ModelAdmin):
         else:
             return qs
 
-    def get_readonly_fields(self, request, obj):
+    def get_readonly_fields(self, request, member):
         if not request.user.has_perm('users.add_user'):
             return ('sector', 'parent', 'membership', 'mc_expiry', 'mc_state',
                 'settled')
         else:
             return ()
+
+    def get_fieldsets(self, request, member):
+        if member.parent:
+            fieldsets = (
+                ('', {'fields':('sector', 'parent')}),
+                ('Anagrafica', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('gender', 'date_of_birth', 'place_of_birth',
+                    'nationality', 'fiscal_code')}),
+                ('Corso/Tesseramento', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('course', 'course_alt', 'course_membership',)}),
+                ('Uploads', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('sign_up', 'privacy', 'med_cert',)}),
+                ('Amministrazione', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('membership', 'mc_expiry', 'mc_state',
+                    'settled')}),
+                )
+            return fieldsets
+        if member.sector == '0-NO':
+            fieldsets = (
+                ('', {'fields':('sector', 'parent')}),
+                ('Contatti', {'classes': ('grp-collapse grp-open',),
+                    'fields':('address', 'phone', 'email_2', 'fiscal_code')}),
+                )
+            return fieldsets
+        elif member.sector == '1-YC':
+            fieldsets = (
+                ('', {'fields':('sector', 'parent')}),
+                ('Anagrafica', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('gender', 'date_of_birth', 'place_of_birth',
+                    'nationality', 'fiscal_code')}),
+                ('Contatti', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('address', 'phone', 'email_2')}),
+                ('Corso/Tesseramento', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('course', 'course_alt', 'course_membership',
+                    )}),
+                ('Uploads', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('sign_up', 'privacy', 'med_cert',)}),
+                ('Amministrazione', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('membership', 'mc_expiry', 'mc_state',
+                    'settled')}),
+                )
+            return fieldsets
+        elif member.sector == '2-NC':
+            fieldsets = (
+                ('', {'fields':('sector', 'parent')}),
+                ('Anagrafica', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('gender', 'date_of_birth', 'place_of_birth',
+                    'nationality', 'fiscal_code')}),
+                ('Contatti', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('address', 'phone', 'email_2')}),
+                ('', {'fields':('no_course_membership', )}),
+                ('Uploads', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('sign_up', 'privacy', 'med_cert',)}),
+                ('Amministrazione', {'classes': ('grp-collapse grp-closed',),
+                    'fields':('membership', 'mc_expiry', 'mc_state',
+                    'settled')}),
+                )
+            return fieldsets
+        else:
+            fieldsets = super().get_fieldsets(request, member)
+            return fieldsets
 
 @admin.register(CourseSchedule)
 class CourseScheduleAdmin(admin.ModelAdmin):
