593edcb3c0291a122d46249b0cc3b26d078026bf
andywar65 <andy.war1965@gmail.com>
2019-11-14 00:11:41 +0100

    adding clean method to change member form

--- users/forms.py
+++ users/forms.py
@@ -6,6 +6,17 @@ class ChangeMemberForm(ModelForm):
     parent = forms.ModelChoiceField(label="Genitore", required = False,
         queryset = User.objects.filter(member__parent = None,
         is_active = True), help_text = 'Solo se minore')
+
+    def clean(self):
+        cd = super().clean()
+        sector = cd.get('sector')
+        parent = cd.get('parent')
+        if not sector == '1-YC' and parent:
+            self.add_error('sector', forms.ValidationError(
+                'Se è minore segue un corso!',
+                code='juvenile_follows_course',
+            ))
+
     class Meta:
         model = Member
         fields = '__all__'
