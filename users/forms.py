47d149affa5e4bf35f86efa0b8d86e1cc457ae6f
andywar65 <andy.war1965@gmail.com>
2019-11-13 23:36:09 +0100

    fixed parent queryset in change member form

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,11 @@
 from django import forms
 from django.forms import ModelForm
-from .models import Member
+from .models import Member, User
 
 class ChangeMemberForm(ModelForm):
     parent = forms.ModelChoiceField(label="Genitore", required = False,
-        queryset = Member.objects.filter(parent = None, user__is_active = True),
-        help_text = 'Solo se minore')
+        queryset = User.objects.filter(member__parent = None,
+        is_active = True), help_text = 'Solo se minore')
     class Meta:
         model = Member
         fields = '__all__'
