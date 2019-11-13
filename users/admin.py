03cd55f96ab16ca049545d22fbd8d1c7d1426d98
andywar65 <andy.war1965@gmail.com>
2019-11-13 22:42:48 +0100

    adding a change member form to process clean data

--- users/admin.py
+++ users/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.db.models import Q
 from django.contrib.auth.admin import UserAdmin
 from .models import User, Member, CourseSchedule, MemberPayment
+from .forms import ChangeMemberForm
 
 admin.site.register(User, UserAdmin)
 
@@ -12,26 +13,11 @@ class MemberPaymentInline(admin.TabularInline):
 
 @admin.register(Member)
 class MemberAdmin(admin.ModelAdmin):
+    form = ChangeMemberForm
     list_display = ('get_full_name', 'sector', 'parent', 'mc_state', 'settled')
     list_filter = ('mc_state', 'settled')
     search_fields = ('user__first_name', 'user__last_name',
         'user__username', 'fiscal_code', 'address')
-    fieldsets = (
-        ('', {'fields':('sector', 'parent')}),
-        ('Anagrafica', {'classes': ('grp-collapse grp-closed',),
-            'fields':('gender', 'date_of_birth', 'place_of_birth',
-            'nationality', 'fiscal_code')}),
-        ('Contatti', {'classes': ('grp-collapse grp-closed',),
-            'fields':('address', 'phone', 'email_2')}),
-        ('Corso/Tesseramento', {'classes': ('grp-collapse grp-closed',),
-            'fields':('course', 'course_alt', 'course_membership',
-            'no_course_membership')}),
-        ('Uploads', {'classes': ('grp-collapse grp-closed',),
-            'fields':('sign_up', 'privacy', 'med_cert',)}),
-        ('Amministrazione', {'classes': ('grp-collapse grp-closed',),
-            'fields':('membership', 'mc_expiry', 'mc_state',
-            'settled')}),
-        )
     inlines = [ MemberPaymentInline, ]
 
     def get_queryset(self, request):
--- users/forms.py
+++ users/forms.py
@@ -1,3 +1,11 @@
 from django import forms
+from django.forms import ModelForm
+from .models import Member
 
-#add forms here
+class ChangeMemberForm(ModelForm):
+    parent = forms.ModelChoiceField(label="Genitore", required = False,
+        queryset = Member.objects.filter(parent = None),
+        help_text = 'Solo se minore')
+    class Meta:
+        model = Member
+        fields = '__all__'
