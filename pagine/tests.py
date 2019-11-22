e0b3489b4a1b40639d4720c054cc712b49240abc
andywar65 <andy.war1965@gmail.com>
2019-11-22 14:51:13 +0100

    adding my first test case!

--- pagine/tests.py
+++ pagine/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import CourseSchedule2
 
-# Create your tests here.
+class CourseSchedule2TestCase(TestCase):
+    def setUp(self):
+        CourseSchedule2.objects.create(full="Lunedì 16:00", abbrev="1-LU16")
+
+    def test_name(self):
+        """Animals that can speak are correctly identified"""
+        orario = CourseSchedule2.objects.get(abbrev="1-LU16")
+        self.assertEqual(orario.__str__(), 'Lunedì 16:00')
