3133fb1aaef8e677de8f6c84dece2a5f864240a7
andywar65 <andy.war1965@gmail.com>
2019-11-24 22:56:29 +0100

    adding image field to location model

--- pagine/models.py
+++ pagine/models.py
@@ -5,7 +5,16 @@ from datetime import datetime
 from django.db import models
 from django.utils.html import format_html
 from django.utils.text import slugify
-from ckeditor_uploader.fields import RichTextUploadingField
+#from ckeditor_uploader.fields import RichTextUploadingField
+
+def date_directory_path(instance, filename):
+    if instance.date:
+        now = instance.date
+    else:
+        now = datetime.now()
+    year = now.strftime("%Y")
+    month = now.strftime("%m")
+    return 'uploads/{0}/{1}/{2}'.format(year, month, filename)
 
 def generate_unique_slug(klass, field):
     """
@@ -34,63 +43,6 @@ class CourseSchedule(models.Model):
     class Meta:
         verbose_name = 'Orario'
         verbose_name_plural = 'Orari'
-
-class Location(models.Model):
-    title = models.CharField('Titolo',
-        help_text='Il nome del luogo',
-        max_length = 50)
-    slug = models.SlugField(max_length=50, unique=True)
-    address = models.CharField('Indirizzo', max_length = 200,
-        help_text = 'Via/Piazza, civico, CAP, Città',)
-    gmap_link = models.URLField('Link di Google Map',
-        blank= True, null=True,
-        help_text="Dal menu di Google Maps seleziona 'Condividi/link', \
-                   copia il link e incollalo qui",
-    )
-    gmap_embed = models.TextField('Incorpora Google Map',
-        blank= True, null=True, max_length=500,
-        help_text="Dal menu di Google Maps seleziona 'Condividi/incorpora', \
-                   copia il link e incollalo qui",
-    )
-    body = RichTextUploadingField('Descrizione', )
-
-    def save(self, *args, **kwargs):
-        if not self.gmap_embed.startswith('http'):
-            # thanks to geeksforgeeks.com! findall returns a list
-            list = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.gmap_embed)
-            if list:
-                self.gmap_embed = list[0]
-        if self.slug:  # edit
-            if slugify(self.title) != self.slug:
-                self.slug = generate_unique_slug(Location, self.title)
-        else:  # create
-            self.slug = generate_unique_slug(Location, self.title)
-        super(Location, self).save(*args, **kwargs)
-
-    def get_gmap_link(self):
-        if self.gmap_link:
-            link = format_html('<a href="{}" class="btn" target="_blank">Mappa</a>',
-                self.gmap_link)
-        else:
-            link = '-'
-        return link
-    get_gmap_link.short_description = 'Link di Google Maps'
-
-    def __str__(self):
-        return self.title
-
-    class Meta:
-        verbose_name = 'Luogo'
-        verbose_name_plural = 'Luoghi'
-
-def date_directory_path(instance, filename):
-    if instance.date:
-        now = instance.date
-    else:
-        now = datetime.now()
-    year = now.strftime("%Y")
-    month = now.strftime("%m")
-    return 'uploads/{0}/{1}/{2}'.format(year, month, filename)
 
 class ImageEntry(models.Model):
     date = models.DateTimeField(blank=True, null=True, editable=False)
@@ -132,3 +84,53 @@ class ImageEntry(models.Model):
     class Meta:
         verbose_name = 'Immagine'
         verbose_name_plural = 'Immagini'
+
+class Location(models.Model):
+    title = models.CharField('Titolo',
+        help_text='Il nome del luogo',
+        max_length = 50)
+    slug = models.SlugField(max_length=50, unique=True)
+    image = models.ForeignKey(ImageEntry, on_delete=models.SET_NULL,
+        blank= True, null=True,)
+    address = models.CharField('Indirizzo', max_length = 200,
+        help_text = 'Via/Piazza, civico, CAP, Città',)
+    gmap_link = models.URLField('Link di Google Map',
+        blank= True, null=True,
+        help_text="Dal menu di Google Maps seleziona 'Condividi/link', \
+                   copia il link e incollalo qui",
+    )
+    gmap_embed = models.TextField('Incorpora Google Map',
+        blank= True, null=True, max_length=500,
+        help_text="Dal menu di Google Maps seleziona 'Condividi/incorpora', \
+                   copia il link e incollalo qui",
+    )
+    body = models.TextField('Descrizione', blank= True, null=True,)
+
+    def save(self, *args, **kwargs):
+        if not self.gmap_embed.startswith('http'):
+            # thanks to geeksforgeeks.com! findall returns a list
+            list = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', self.gmap_embed)
+            if list:
+                self.gmap_embed = list[0]
+        if self.slug:  # edit
+            if slugify(self.title) != self.slug:
+                self.slug = generate_unique_slug(Location, self.title)
+        else:  # create
+            self.slug = generate_unique_slug(Location, self.title)
+        super(Location, self).save(*args, **kwargs)
+
+    def get_gmap_link(self):
+        if self.gmap_link:
+            link = format_html('<a href="{}" class="btn" target="_blank">Mappa</a>',
+                self.gmap_link)
+        else:
+            link = '-'
+        return link
+    get_gmap_link.short_description = 'Link di Google Maps'
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Luogo'
+        verbose_name_plural = 'Luoghi'
