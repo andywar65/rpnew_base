cd15dbf616775816772e92328c4417caba12f82c
andywar65 <andy.war1965@gmail.com>
2019-11-27 23:08:40 +0100

    unable to do tests with two models

--- pagine/tests.py
+++ pagine/tests.py
@@ -1,5 +1,6 @@
+from datetime import datetime
 from django.test import TestCase
-from .models import (CourseSchedule, Location)
+from .models import (CourseSchedule, Location, ImageEntry)
 
 class CourseScheduleTestCase(TestCase):
     def setUp(self):
@@ -11,10 +12,19 @@ class CourseScheduleTestCase(TestCase):
         self.assertEqual(orario.__str__(), 'Lunedì 16:00')
 
 class LocationTestCase(TestCase):
+    #@classmethod
+    #def setUpTestData(cls):
     def setUp(self):
+        # Set up data for the whole TestCase
+        #cls.img = ImageEntry.objects.create(name='IMG', image='image.jpg',
+            #thumb='image_thumb.jpg',
+            #description = 'Description')
         Location.objects.create(title="Marco",
+            #image = cls.img,
             address="Via Agnelli",
-            gmap_link = 'https://goo.gl/maps/jLKe3iur2EtL8zuB7')
+            gmap_link = 'https://goo.gl/maps/jLKe3iur2EtL8zuB7',
+            gmap_embed = '<scrap>https://goo.gl/maps/jLKe3iur2EtL8zuB7</scrap>',
+            body = 'Body')
 
     def test_name(self):
         """Name is correctly identified"""
@@ -26,3 +36,9 @@ class LocationTestCase(TestCase):
         luogo = Location.objects.get(title="Marco")
         self.assertEqual(luogo.get_gmap_link(),
             '<a href="https://goo.gl/maps/jLKe3iur2EtL8zuB7" class="btn" target="_blank">Mappa</a>')
+
+    def test_thumb(self):
+        """Thumbnail is correctly identified"""
+        luogo = Location.objects.get(title="Marco")
+        self.assertEqual(luogo.get_thumb(),
+            '')
