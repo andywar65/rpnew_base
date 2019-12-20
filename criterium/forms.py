cc5b670171d7069fb58d7684a07e21c787b80280
andywar65 <andy.war1965@gmail.com>
2019-12-20 16:03:14 +0100

    race form validates if date and event are null

--- criterium/forms.py
+++ criterium/forms.py
@@ -4,8 +4,16 @@ from .models import Race
 from pagine.models import Event
 
 class RaceForm(ModelForm):
-    event = forms.ModelChoiceField(label="Event", required = False,
+    event = forms.ModelChoiceField(label="Evento", required = False,
         queryset = Event.objects.filter(tags__name__in = ['criterium'], ), )
+
+    def clean_date(self):
+        date = self.cleaned_data['date']
+        event = self.cleaned_data['event']
+        if not date and not event:
+            msg = 'Senza evento occorre inserire almeno la data.'
+            raise forms.ValidationError(msg)
+        return date
 
     class Meta:
         model = Race
--- criterium/views.py
+++ criterium/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import (DetailView, )
 from .models import (Race, )
-from .forms import RaceForm
 
 class DetailRace(DetailView):
     model = Race
