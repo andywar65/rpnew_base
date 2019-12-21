50e541be918a3264cfbe38c76949e0db3347d1a4
andywar65 <andy.war1965@gmail.com>
2019-12-21 12:07:26 +0100

    adding race detail template

--- criterium/views.py
+++ criterium/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.views.generic import (DetailView, )
 from django.views.generic.dates import (ArchiveIndexView, YearArchiveView, )
-from .models import (Race, )
+from .models import (Race, Athlete, )
 
 class DetailRace(DetailView):
     model = Race
     context_object_name = 'race'
     slug_field = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        race = context['object']
+        athletes = Athlete.objects.filter(race_id=race.id)
+        context['females'] = athletes.filter(user__member__gender='F').order_by('-points')
+        context['males'] = athletes.filter(user__member__gender='M').order_by('-points')
+        return context
 
 class RaceYearArchiveView(YearArchiveView):
     model = Race
