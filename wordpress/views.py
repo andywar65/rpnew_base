cde51a79a3e3b999a9fec432fadb33fcfac5cacf
andywar65 <andy.war1965@gmail.com>
2019-12-30 16:28:58 +0100

    adding list view to fetch articles from wp site

--- wordpress/views.py
+++ wordpress/views.py
@@ -1,5 +1,11 @@
 import requests
 from django.shortcuts import render
 
+target = 'https://rifondazionepodistica.it/wp-json/wp/v2/'
+
 def wp_list_view(request):
-    return
+    response = requests.get(target + 'posts?per_page=1')
+    posts = response.json()
+    title = posts[0]['title']['rendered']
+    return render(request, 'wordpress/list.html', {'title': title,
+    })
