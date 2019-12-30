ef4faad08cc6039d17f7edbe18e80249da4a1012
andywar65 <andy.war1965@gmail.com>
2019-12-30 19:11:10 +0100

    featured image loaded in wp list template

--- wordpress/views.py
+++ wordpress/views.py
@@ -4,15 +4,19 @@ from django.shortcuts import render
 target = 'https://rifondazionepodistica.it/wp-json/wp/v2/'
 
 def wp_list_view(request):
-    filter = 'posts?_fields=title,link,excerpt&per_page=12'
-    response = requests.get(target + filter )
+    filter = {
+        '_fields': 'id,title,excerpt,jetpack_featured_media_url',
+        'per_page': 12,
+        }
+    response = requests.get(target + 'posts', params = filter )
     wp_posts = response.json()
     posts = []
     for wp_post in wp_posts:
         post = {}
-        post['link'] = wp_post['link']
+        post['id'] = wp_post['id']
         post['title'] = wp_post['title']['rendered']
         post['excerpt'] = wp_post['excerpt']['rendered']
+        post['image'] = wp_post['jetpack_featured_media_url']
         posts.append(post)
     return render(request, 'wordpress/list.html', {'posts': posts,
     })
