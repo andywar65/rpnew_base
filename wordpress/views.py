6a2df26c01145522180edf17a84f9f91b43de982
andywar65 <andy.war1965@gmail.com>
2019-12-30 19:17:50 +0100

    next and previous pages are passed to wp list template

--- wordpress/views.py
+++ wordpress/views.py
@@ -4,9 +4,13 @@ from django.shortcuts import render
 target = 'https://rifondazionepodistica.it/wp-json/wp/v2/'
 
 def wp_list_view(request):
+    page = 1
+    if 'page' in request.GET:
+        page = request.GET['page']
     filter = {
         '_fields': 'id,title,excerpt,jetpack_featured_media_url',
         'per_page': 12,
+        'page': page,
         }
     response = requests.get(target + 'posts', params = filter )
     wp_posts = response.json()
@@ -18,5 +22,6 @@ def wp_list_view(request):
         post['excerpt'] = wp_post['excerpt']['rendered']
         post['image'] = wp_post['jetpack_featured_media_url']
         posts.append(post)
-    return render(request, 'wordpress/list.html', {'posts': posts,
+    return render(request, 'wordpress/list.html', {'posts': posts, 'page': page,
+    'previous': page-1, 'next': page+1
     })
