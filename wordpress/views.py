0bac54bea909932e24673af2ffefec7c18437e13
andywar65 <andy.war1965@gmail.com>
2019-12-30 18:56:50 +0100

    wordpress list view shows first 12 articles as cards

--- wordpress/views.py
+++ wordpress/views.py
@@ -4,8 +4,15 @@ from django.shortcuts import render
 target = 'https://rifondazionepodistica.it/wp-json/wp/v2/'
 
 def wp_list_view(request):
-    response = requests.get(target + 'posts?per_page=1')
-    posts = response.json()
-    title = posts[0]['title']['rendered']
-    return render(request, 'wordpress/list.html', {'title': title,
+    filter = 'posts?_fields=title,link,excerpt&per_page=12'
+    response = requests.get(target + filter )
+    wp_posts = response.json()
+    posts = []
+    for wp_post in wp_posts:
+        post = {}
+        post['link'] = wp_post['link']
+        post['title'] = wp_post['title']['rendered']
+        post['excerpt'] = wp_post['excerpt']['rendered']
+        posts.append(post)
+    return render(request, 'wordpress/list.html', {'posts': posts,
     })
