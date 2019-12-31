b2868fb6c5669ef6ccd059da725c00b0d1a54d54
andywar65 <andy.war1965@gmail.com>
2019-12-31 12:49:56 +0100

    adding wp detail template

--- wordpress/views.py
+++ wordpress/views.py
@@ -34,19 +34,23 @@ def wp_list_view(request):
 def wp_detail_view(request, id):
     filter = {
         'id': id,
-        '_fields': 'title,content,jetpack_featured_media_url',
+        '_fields': 'title,content,jetpack_featured_media_url,date,author,categories',
         }
-    response = requests.get(target + 'posts', params = filter )
-    wp_posts = response.json()
-    wp_post = wp_posts[0]
+    response = requests.get(target + 'posts/' + str(id), params = filter )
+    wp_post = response.json()
+    #here we have to go back and fetch categories and author, or we
+    #hardcode them, as they never change
     post = {}
     post['title'] = wp_post['title']['rendered']
+    post['date'] = wp_post['date']
+    post['author'] = wp_post['author']
+    post['categories'] = wp_post['categories']
     post['content'] = wp_post['content']['rendered']
     if wp_post['content']['protected'] == 'true':
         post['visible'] = False
     else:
         post['visible'] = True
     post['image'] = wp_post['jetpack_featured_media_url']
-    assert False
+    #assert False
     return render(request, 'wordpress/wp_detail.html', {'post': post,
     })
