554dca667f766859bbdc6667930784de9697d003
andywar65 <andy.war1965@gmail.com>
2019-12-31 15:29:58 +0100

    posts can be filtered for category

--- wordpress/urls.py
+++ wordpress/urls.py
@@ -4,5 +4,6 @@ from .views import (wp_list_view, wp_detail_view)
 app_name = 'wordpress'
 urlpatterns = [
     path('', wp_list_view, name = 'posts'),
+    path('categoria/<int:category>', wp_list_view, name = 'posts'),
     path('<int:id>/', wp_detail_view, name = 'post'),
     ]
--- wordpress/views.py
+++ wordpress/views.py
@@ -9,6 +9,11 @@ def get_user_name(id):
     author = response.json()
     return author['name']
 
+def get_category_name(id):
+    response = requests.get(target + 'categories/' + str(id) )
+    category = response.json()
+    return category['name']
+
 def get_category_dict():
     response = requests.get(target + 'categories/', params = {'per_page': 100})
     all_cat = response.json()
@@ -18,32 +23,35 @@ def get_category_dict():
     return category_dict
 
 
-def wp_list_view(request):
+def wp_list_view(request, category=None):
     if 'page' in request.GET:
         page = int(request.GET['page'])
     else:
         page = 1
     filter = {
+        'categories': category,
         '_fields': 'id,title,excerpt,jetpack_featured_media_url',
         'per_page': 9,
         'page': page,
         }
     response = requests.get(target + 'posts', params = filter )
     wp_posts = response.json()
+    if category:
+        category = get_category_name(category)
     posts = []
     for wp_post in wp_posts:
         post = {}
         post['id'] = wp_post['id']
         post['title'] = wp_post['title']['rendered']
         post['excerpt'] = wp_post['excerpt']['rendered']
-        if wp_post['excerpt']['protected'] == 'true':
+        if wp_post['excerpt']['protected'] == True:
             post['visible'] = False
         else:
             post['visible'] = True
         post['image'] = wp_post['jetpack_featured_media_url']
         posts.append(post)
     return render(request, 'wordpress/wp_list.html', {'posts': posts, 'page': page,
-    'previous': page-1, 'next': page+1
+    'previous': page-1, 'next': page+1, 'category': category,
     })
 
 def wp_detail_view(request, id):
@@ -65,7 +73,7 @@ def wp_detail_view(request, id):
             categories[cat] = category_dict[cat]
             post['categories'] = categories
     post['content'] = wp_post['content']['rendered']
-    if wp_post['content']['protected'] == 'true':
+    if wp_post['content']['protected'] == True:
         post['visible'] = False
     else:
         post['visible'] = True
