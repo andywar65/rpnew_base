9bd99157c0aadafb5eef6ef6070e0282d662ae7f
andywar65 <andy.war1965@gmail.com>
2020-01-01 22:59:16 +0100

    event upgrades added to search results

--- rpnew_prog/views/search.py
+++ rpnew_prog/views/search.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.db.models import Q
-from pagine.models import (Event, Blog, )
+from pagine.models import (Event, Blog, EventUpgrade, )
 
 def search_results(request):
     success = False
     if 'q' in request.GET:
         q = request.GET['q']
+        upgrades = []
+        upgrades = EventUpgrade.objects.filter(Q(title__icontains=q)|
+            Q(body__icontains=q)).values_list('event_id', flat = True)
         events = Event.objects.filter(Q(title__icontains=q)|
             Q(intro__icontains=q)|Q(body__icontains=q)|
-            Q(chronicle__icontains=q)|Q(restricted__icontains=q))
+            Q(chronicle__icontains=q)|Q(restricted__icontains=q)|
+            Q(id__in = upgrades))
         if events:
             success = True
         blogs = Blog.objects.filter(Q(title__icontains=q)|
