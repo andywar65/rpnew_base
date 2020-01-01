b91ce9f09fd2136103432b8f773de6864802dc6d
andywar65 <andy.war1965@gmail.com>
2020-01-01 18:54:44 +0100

    added blogs to search

--- rpnew_prog/views/search.py
+++ rpnew_prog/views/search.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from django.db.models import Q
-from pagine.models import (Event, )
+from pagine.models import (Event, Blog, )
 
 def search_results(request):
     success = False
     if 'q' in request.GET:
         q = request.GET['q']
         events = Event.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q))
+            Q(intro__icontains=q)|Q(body__icontains=q)|
+            Q(chronicle__icontains=q)|Q(restricted__icontains=q))
         if events:
             success = True
+        blogs = Blog.objects.filter(Q(title__icontains=q)|
+            Q(intro__icontains=q)|Q(body__icontains=q))
+        if blogs:
+            success = True
         return render(request, 'search_results.html', {'search': q,
-            'all_events': events, 'success': success})
+            'all_events': events, 'all_blogs': blogs, 'success': success})
     else:
         return render(request, 'search_results.html', {'success': success, })
