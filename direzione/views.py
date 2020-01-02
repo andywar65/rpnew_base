2956a4f703df809312a166619f1a4a72d59f3657
andywar65 <andy.war1965@gmail.com>
2020-01-02 18:23:12 +0100

    added uploads to convention detail template

--- direzione/views.py
+++ direzione/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import (ListView, DetailView, TemplateView)
-from .models import (Convention)
+from .models import (Convention, ConventionUpload, )
 
 class ConventionListView(ListView):
     model = Convention
@@ -12,3 +12,10 @@ class ConventionDetailView(DetailView):
     model = Convention
     context_object_name = 'conv'
     slug_field = 'slug'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        conv = context['object']
+        uploads = ConventionUpload.objects.filter(convention_id=conv.id)
+        context['uploads'] = uploads
+        return context
