e46f17534f6d3d467e1567a1101c710722b5012d
andywar65 <andy.war1965@gmail.com>
2020-03-04 18:37:05 +0100

    added upgrade stream to event model

--- pagine/models.py
+++ pagine/models.py
@@ -111,8 +111,10 @@ class Event(models.Model):
     intro = models.CharField('Introduzione',
         default = 'Un altro appuntamento con RP!', max_length = 100)
     stream = StreamField(model_list=[ IndexedParagraph, CaptionedImage,
-        Gallery, DownloadableFile, LinkableList, BoxedText, EventUpgrade],
+        Gallery, DownloadableFile, LinkableList, BoxedText, EventUpgrade, ],
         verbose_name="Lancio")
+    upgrade_stream = StreamField(model_list=[ EventUpgrade, ],
+        verbose_name="Aggiornamenti")
     chron_stream = StreamField(model_list=[ IndexedParagraph, CaptionedImage,
         Gallery, DownloadableFile, LinkableList, BoxedText],
         verbose_name="Cronaca")
