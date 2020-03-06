8473aa35dc8d9362f17d5e1f3c838ba64852e8cb
andywar65 <andy.war1965@gmail.com>
2020-03-06 17:03:19 +0100

    skipping some fields in search view

--- rpnew_prog/views/search.py
+++ rpnew_prog/views/search.py
@@ -1,7 +1,7 @@
 from django import forms
 from django.shortcuts import render
 from django.db.models import Q
-from pagine.models import (Event, Blog, EventUpgrade, UserUpload)
+from pagine.models import (Event, Blog, UserUpload)
 
 class ValidateForm(forms.Form):
     q = forms.CharField(max_length=100)
@@ -13,17 +13,17 @@ def search_results(request):
         q = request.GET['q']
         uploads = UserUpload.objects.filter(body__icontains = q)
         up_list = uploads.values_list('event_id', flat = True)
-        upgrades = EventUpgrade.objects.filter(Q(title__icontains=q)|
-            Q(body__icontains=q)).values_list('event_id', flat = True)
+        #upgrades = EventUpgrade.objects.filter(Q(title__icontains=q)|
+            #Q(body__icontains=q)).values_list('event_id', flat = True)
         events = Event.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(body__icontains=q)|
-            Q(chronicle__icontains=q)|Q(restricted__icontains=q)|
-            Q(id__in = upgrades)|Q(id__in = up_list))
+            Q(intro__icontains=q)|Q(id__in = up_list))#Q(body__icontains=q)|
+            #Q(chronicle__icontains=q)|Q(restricted__icontains=q)|
+            #Q(id__in = upgrades)|
         if events:
             success = True
         up_list = uploads.values_list('post_id', flat = True)
         blogs = Blog.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(body__icontains=q)|Q(id__in = up_list))
+            Q(intro__icontains=q)|Q(id__in = up_list))#Q(body__icontains=q)|
         if blogs:
             success = True
         return render(request, 'search_results.html', {'search': q,
