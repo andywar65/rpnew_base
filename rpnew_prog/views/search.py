5fbe1b23dc49b2e1092442f4d9ea4198953793cb
andywar65 <andy.war1965@gmail.com>
2020-03-07 16:26:07 +0100

    in events and blogs too

--- rpnew_prog/views/search.py
+++ rpnew_prog/views/search.py
@@ -29,10 +29,16 @@ def search_results(request):
             Q(intro__icontains=q)|Q(id__in = up_list)|Q(id__in = ev_list))
         if events:
             success = True
+        #blog content type
+        bl_type = ContentType.objects.get(app_label='pagine', model='blog').id
+        #filter paragraphs by blog type
+        bl_paragraphs = paragraphs.filter( parent_type = bl_type )
+        #extract list of blogs
+        bl_list = bl_paragraphs.values_list('parent_id', flat = True)
         #prepare list of user uploads referenced by blog posts
         up_list = uploads.values_list('post_id', flat = True)
         blogs = Blog.objects.filter(Q(title__icontains=q)|
-            Q(intro__icontains=q)|Q(id__in = up_list))
+            Q(intro__icontains=q)|Q(id__in = up_list)|Q(id__in = bl_list))
         if blogs:
             success = True
         return render(request, 'search_results.html', {'search': q,
